ca92dd5e4b5b2cfb890ead9715e1eb2980531096
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-11 08:14:10 -0600

    feat: Completed functional app

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,60 @@
- 
+ document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+ // Calculate results
+ function calculateResults(e){
+  console.log('Calculating . . . ');
+
+  // UI variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payments
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+  e.preventDefault;
+
+  if(isFinite(monthly)){
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    showError('Please check your input numbers');
+  }
+
+  e.preventDefault();
+ }
+
+ // Show error 
+ function showError(error){
+  // Create an html div
+  const errorDiv = document.createElement('div');
+
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create textnode and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear erro after 3 seconds
+  setTimeout(clearError, 3000);
+ }
+
+ // Clear error
+ function clearError(){
+  document.querySelector('.alert').remove();
+ }
